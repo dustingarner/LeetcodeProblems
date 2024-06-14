264a1eb520dd4e306850c1379ca74a1f62c89467
Dustin Garner <144393035+dustingarner@users.noreply.github.com>
2024-06-14 12:33:21 -0500

    Create Leetcode230.cpp

---
+++ Leetcode230.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int left = getNodeCount(root->left);
+        if(left == k-1){return root->val;}
+        if(left >= k){return kthSmallest(root->left, k);}
+        return kthSmallest(root->right, k-left-1);
+    }
+
+    int getNodeCount(TreeNode* root){
+        if(root == nullptr){return 0;}
+        return 1 + getNodeCount(root->left) + getNodeCount(root->right);
+    }
+};
