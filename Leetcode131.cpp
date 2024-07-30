5e82d5c639cb56d5d541e97c7a8f4a5489d52c63
Dustin Garner <144393035+dustingarner@users.noreply.github.com>
2024-07-30 16:13:58 -0500

    Update Leetcode131.cpp

--- Leetcode131.cpp
+++ Leetcode131.cpp
@@ -2,58 +2,38 @@ class Solution {
 public:
     vector<vector<string>> partition(string& s, int index = 0) {
         vector<vector<string>> part;
-        if(index==s.length()){
-            return part;
-        }
-        for(int i=index; i<s.length(); ++i){
-            if(isOddPal(s, i, index)){
-                string tempWord = s.substr(index, ((i-index)*2)+1);
-                vector<vector<string>> tempPart = partition(s, (2*i-index+1));
-                for(int j=0; j<tempPart.size(); ++j){
-                    tempPart[j].insert(tempPart[j].begin(), tempWord);
-                }
-                part.insert(part.begin(), tempPart.begin(), tempPart.end());
-                if(tempPart.size()==0){
-                    part.insert(part.begin(), vector<string> (1, tempWord));
-                }
-            }
-            if(isEvenPal(s, i, index)){
-                string tempWord = s.substr(index, ((i-index)*2)+2);
-                vector<vector<string>> tempPart = partition(s, (2*i-index+2));
-                for(int j=0; j<tempPart.size(); ++j){
-                    tempPart[j].insert(tempPart[j].begin(), tempWord);
-                }
-                part.insert(part.begin(), tempPart.begin(), tempPart.end());
-                if(tempPart.size()==0){
-                    part.insert(part.begin(), vector<string> (1, tempWord));
-                }
-            }
-        }
+        vector<string> palindromes;
+        backtrack(part, palindromes, s, 0, 1);
         return part;
     }
 
-    bool isOddPal(string& s, int index, int startIndex){
-        int tempIndex = index;
-        for(int i=1; i<=index-startIndex; ++i){
-            if(index+i==s.length()){
-                return false;
-            }
-            if(s[index-i]!=s[index+i]){
-                return false;
-            }
+    void backtrack(vector<vector<string>>& part, vector<string>& palindromes, string& s, int start, int length){
+        if(start==s.length()){
+            part.push_back(palindromes);
+            return;
         }
-        return true;
+        if(start+length-1==s.length()){
+            return;
+        }
+        backtrack(part, palindromes, s, start, length+1);
+        if(isPalindrome(start, length, s)){
+            palindromes.push_back(s.substr(start, length));
+            backtrack(part, palindromes, s, start+length, 1);
+            palindromes.pop_back();
+        }
     }
 
-    bool isEvenPal(string& s, int index, int startIndex){
-        int tempIndex = index;
-        for(int i=0; i<=index-startIndex; ++i){
-            if(index+1+i==s.length()){
+    bool isPalindrome(int start, int length, string& s){
+        if(start+length-1 == s.length()){
+            return false;
+        }
+        int end = start+length-1;
+        while(start < end){
+            if(s[start]!=s[end]){
                 return false;
             }
-            if(s[index-i]!=s[index+1+i]){
-                return false;
-            }
+            start++;
+            end--;
         }
         return true;
     }
